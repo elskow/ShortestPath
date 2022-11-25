31931c033b2fc9992399ee90adc6b1ddf6de09f3
NaufalF121 <60810604+NaufalF121@users.noreply.github.com>
2022-11-25 19:36:33 +0700

    new file:   .gitignore modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+int MAX_W = 5000
+int MAX_p = 40;
+struct data {
+    string nama;
+    int berat_b;
+    int berat_t;
+    int harga;
+
+}
+vector <data> ingfo;
 
 void clearscreen(){
     #ifdef _WIN32
@@ -31,14 +41,82 @@ void display_kursi(){
 }
 void informasi(){
     cout << "########################## INFORMASI ##########################" << endl;
-    cout << "1. Harga tiket pesawat Rp. 1.000.000" << endl;
+    cout << "1. Harga kursi Ekonomiu Rp. 1.000.000" << endl;
     cout << "2. Harga kursi VIP Rp. 1.500.000" << endl;
     cout << "3. Harga kursi VVIP Rp. 2.000.000" << endl;
     cout << "Apabila Kursi sudah penuh saat pemesanan tiket akan dihubungi sebelum penerbangan " << endl;
 
 }
 
+void pemesanan(){
+    cout << "=================Pemesanan Tiket=================" << endl;
+    cout << "Silahkan pilih menu dibawah ini :       " << endl;
+    cout << "1. Pemesanan Kuris Ekonomi" << endl;
+    cout << "2. Pemesanan Kursi VIP" << endl;
+    cout << "3. Pemesanan Kursi VVIP" << endl;
+    cout << "4. Kembali" << endl;
+    cout << "================================================" << endl;
+    int temp;
+    cin >> temp;
 
+    if(temp == 1){
+        cout << "Pemesanan Kursi Ekonomi" << endl;
+        cout << "Masukkan nama anda : " << endl;
+        string a;
+        cin >> a;
+        ingfo.nama.push_back(a);
+        cout << "Masukkan berat barang anda : " << endl;
+        int b;
+        cin >> b;
+        ingfo.berat_b.push_back(b);
+        cout << "Masukkan berat tubuh anda : " << endl;
+        int c;
+        cin >> c;
+        ingfo.berat_t.push_back(c);
+        cout << "Masukkan harga barang anda : " << endl;
+        int d = 1000000;
+        
+        ingfo.harga.push_back(c);
+        cout << "Pemesanan berhasil" << endl;
+        cout << "Nama : " << a << endl;
+        cout << "Berat Bawaan : " << b << endl;
+        cout << "Harga : " << d << endl;
+        cout << "Untuk kursi anda akan kami hubungi sebelum penerbangan" << endl;
+       
+    }
+    else if(temp == 2){
+        cout << "Pemesanan Kursi VIP" << endl;
+        cout << "Masukkan nama anda : " << endl;
+        string a;
+        cin >> a;
+        ingfo.nama.push_back(a);
+        cout << "Masukkan berat barang anda : " << endl;
+        int b;
+        cin >> b;
+        ingfo.berat_b.push_back(b);
+        cout << "Masukkan berat tubuh anda : " << endl;
+        int c;
+        cin >> c;
+        ingfo.berat_t.push_back(c);
+        cout << "Masukkan harga barang anda : " << endl;
+        int d = 1500000;
+        
+        ingfo.harga.push_back(c);
+        cout << "Pemesanan berhasil" << endl;
+        cout << "Nama : " << a << endl;
+        cout << "Berat Bawaan : " << b << endl;
+        cout << "Harga : " << d << endl;
+        cout << "Untuk kursi anda akan kami hubungi sebelum penerbangan" << endl;
+       
+    }else if(temp == 3){
+        
+    }else if(temp == 4){
+        menu();
+    }else{
+        cout << "Inputan salah" << endl;
+    }
+    
+}
 void menu(){
     cout << "============Selamat Datang di Pesawat===========" << endl;
     cout << "=================PT. Angkasa====================" << endl;
@@ -64,27 +142,10 @@ void menu(){
     }
 }
 
-void pemesanan(){
-    cout << "=================Pemesanan Tiket=================" << endl;
-    cout << "Silahkan pilih menu dibawah ini :       " << endl;
-    cout << "1. Pemesanan Tiket Pesawat" << endl;
-    cout << "2. Pemesanan Kursi VIP" << endl;
-    cout << "3. Pemesanan Kursi VVIP" << endl;
-    cout << "4. Kembali" << endl;
-    cout << "================================================" << endl;
-    int temp;
-    cin >> temp;
-    if(temp == 1){
-        pemesanan_tiket();
-    }else if(temp == 2){
-        pemesanan_kursi_vip();
-    }else if(temp == 3){
-        pemesanan_kursi_vvip();
-    }else if(temp == 4){
-        menu();
-    }else{
-        cout << "Inputan salah" << endl;
-    }
-}
 
+int main(){
+      
+        clearscreen();
+        return 0;
+    }
 
