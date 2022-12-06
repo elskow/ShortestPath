3e384653c433bd0979084492da6f2d174691fbbe
NaufalF121 <60810604+NaufalF121@users.noreply.github.com>
2022-12-06 20:51:53 +0700

    Update main_iterative.cpp

--- main_iterative.cpp
+++ main_iterative.cpp
@@ -8,13 +8,13 @@
 using namespace std;
 
 struct Node {
-    int id;
+    string id;
     int x , y;
 
-    Node(int id , int x , int y) : id(id) , x(x) , y(y) {}
+    Node(string id , int x , int y) : id(id) , x(x) , y(y) {}
 };
-
 vector<Node> nodes;
+vector<int> path;
 
 void clearscreen(){
     #ifdef _WIN32
@@ -27,11 +27,19 @@ void clearscreen(){
 void inputKoordinat(int n){
     int x , y;
     for(int i = 0 ; i < n ; i++){
+        cout << "Masukkan nama penerima paket ke -" <<i +1 << " : ";
+        string temp;
+        getline(cin, temp);
+
         cout << "Masukkan koordinat penerima paket ke-" << i + 1 << " : ";
         cin >> x >> y;
-        nodes.push_back(Node(i + 1 , x , y));
+        cin.ignore(999999,'\n');
+        nodes.push_back(Node(temp , x , y));
     }
 }
+int totaldistance = 0;
+void dijkstra(vector<bool> &visited, vector<Node> &nodes, int current, int totaldistance);
+
 
 int main() {
     clearscreen();
@@ -39,29 +47,43 @@ int main() {
     cout << "\t\tSelamat Datang di Aplikasi Navigasi PergiMakanan" << endl;
     cout << "\t\t==============================================" << endl;
 
-
     int startingnodesx, startingnodesy;
 
     cout << "Silahkan masukkan koordinat awal anda : ";
     cin >> startingnodesx >> startingnodesy;
-
+    cin.ignore(999999,'\n');
     cout << "Masukkan jumlah alamat paket yang akan anda kirimkan : ";
     int n;
     cin >> n;
-
-    nodes.push_back(Node(0, startingnodesx, startingnodesy));
+    cin.ignore(999999,'\n');
+    nodes.push_back(Node("Kurir", startingnodesx, startingnodesy));
 
     inputKoordinat(n);
-
-    nodes.push_back(Node(n + 1, startingnodesx, startingnodesy));
-
+    
     vector<bool> visited(nodes.size(), false);
     visited[0] = true;
     int current = 0;
-    int totaldistance = 0;
+   
 
-    vector<int> path;
-    
+    dijkstra(visited, nodes, current, totaldistance);
+
+    //clearscreen();
+
+  
+    cout << "Rute yang harus ditempuh : " << endl;
+
+    for (int i = 0; i < path.size(); i++) {
+        if (i == 0) {
+            cout << "Titik keberangkatan" << " (" << nodes[i].x << ", " << nodes[i].y << ")" << endl;
+        }
+        cout << "Paket Untuk " << nodes[path[i]].id << " dengan koordinat (" << nodes[path[i]].x << ", " << nodes[path[i]].y << ")" << endl;
+    }
+    cout << "Kembali ke " << "(" << nodes[0].x << ", " << nodes[0].y << ")" << endl;
+
+    return 0;
+}
+
+void dijkstra(vector<bool> &visited, vector<Node> &nodes, int current, int totaldistance){
     while (true) {
         int next = -1;
         int mindistance = INT_MAX;
@@ -78,24 +100,13 @@ int main() {
         }
         if (next == -1) break;
         totaldistance += mindistance;
+        
         path.push_back(next);
         visited[next] = true;
         current = next;
     }
 
     totaldistance += abs(nodes[current].x - nodes[nodes.size() - 1].x) + abs(nodes[current].y - nodes[nodes.size() - 1].y);
-
+    totaldistance += abs(nodes[0].x - nodes[nodes.size() - 1].x) + abs(nodes[0].y - nodes[nodes.size() - 1].y);
     cout << "Jarak total yang harus ditempuh : " << totaldistance << endl;
-    cout << "Rute yang harus ditempuh : " << endl;
-
-    for (int i = 0; i < path.size(); i++) {
-        if (i == 0) {
-            cout << "Titik keberangkatan" << " (" << nodes[path[i]].x << ", " << nodes[path[i]].y << ")" << endl;
-        } else {
-            cout << "Paket ke - " << nodes[path[i]].id << " (" << nodes[path[i]].x << ", " << nodes[path[i]].y << ")" << endl;
-        }
-    }
-    cout << "Kembali ke " << "(" << nodes[0].x << ", " << nodes[0].y << ")" << endl;
-
-    return 0;
 }
