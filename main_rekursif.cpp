1b5d752d6977cfbcc85e3c910e16aaf74c713976
NaufalF121 <60810604+NaufalF121@users.noreply.github.com>
2022-12-07 10:17:56 +0700

    Update main_rekursif.cpp

--- main_rekursif.cpp
+++ main_rekursif.cpp
@@ -45,67 +45,82 @@ void inputKoordinat(int n){
         nodes.push_back(Node(temp , x , y));
     }
 }
-
-int main() {
-    clearscreen();
-
+void header(){
     cout << "\t\tSelamat Datang di Aplikasi Navigasi PergiMakanan" << endl;
     cout << "\t\t==============================================" << endl;
-
-
+}
+int main() {
+    clearscreen();
+    header();
+    
     int startingnodesx, startingnodesy;
 
-    cout << "Silahkan masukkan koordinat awal anda : ";
+    cout << "\nSilahkan masukkan koordinat awal anda : ";
     cin >> startingnodesx >> startingnodesy;
-cin.ignore(999999,'\n');
+
+    cin.ignore(999999,'\n');
     cout << "Masukkan jumlah alamat paket yang akan anda kirimkan : ";
     int n;
     cin >> n;
-cin.ignore(999999,'\n');
+
+    cin.ignore(999999,'\n');
     nodes.push_back(Node("Kurir", startingnodesx, startingnodesy));
 
+    clearscreen();
+    header();
     inputKoordinat(n);
     
     vector<bool> visited(nodes.size(), false);
     visited[0] = true;
+    float totaldistance = 0;
     int current = 0;
-    int totaldistance = 0;
+   
+    clearscreen();
+    header();
 
-    
-    
-    while (true) {
+    while (true)
+    {
         int next = -1;
-        int mindistance = INT_MAX;
+        float mindistance = INT_MAX;
 
         // Find the nearest node
-        for (int i = 0; i < nodes.size(); i++) {
-            if (visited[i]) continue;
+        for (int i = 0; i < nodes.size(); i++)
+        {
+            if (visited[i])
+                continue;
 
-            int distance = abs(nodes[current].x - nodes[i].x) + abs(nodes[current].y - nodes[i].y);
-            if (distance < mindistance) {
+            float distance = sqrt((abs(nodes[current].x - nodes[i].x))^2 + (abs(nodes[current].y - nodes[i].y))^2);
+            if (distance < mindistance)
+            {
                 mindistance = distance;
                 next = i;
             }
         }
-        if (next == -1) break;
-        totaldistance += mindistance;
+        if (next == -1)
+            break;
         
+        // cout << "mindistance : " << mindistance << endl;
+        totaldistance += mindistance;
+
         path.push_back(next);
         visited[next] = true;
         current = next;
+        // totaldistance += abs(nodes[current].x - nodes[nodes.size() - 1].x) + abs(nodes[current].y - nodes[nodes.size() - 1].y);
+        // cout << "totaldistance : " << totaldistance << endl;
     }
 
-    totaldistance += abs(nodes[current].x - nodes[nodes.size() - 1].x) + abs(nodes[current].y - nodes[nodes.size() - 1].y);
-    totaldistance += abs(nodes[0].x - nodes[nodes.size() - 1].x) + abs(nodes[0].y - nodes[nodes.size() - 1].y);
-    cout << "Jarak total yang harus ditempuh : " << totaldistance << endl;
-     
-    
-    
-
-    
-    cout << "Rute yang harus ditempuh : " << endl;
+    totaldistance += sqrt((abs(nodes[0].x - nodes[current].x))^2 + (abs(nodes[0].y - nodes[current].y))^2);
     cout << "Titik keberangkatan" << " (" << nodes[0].x << ", " << nodes[0].y << ")" << endl;
+    cout << "Rute yang harus ditempuh : " << endl;
     output(path.size()-1);
+    if ((int(ceil(totaldistance)) % int(floor(totaldistance))) == 0){
+        cout << "Jarak total yang harus ditempuh : " << int(totaldistance) << endl;
+        return 0;
+    } 
+
+    cout << "Jarak total yang harus ditempuh : " << fixed << setprecision(2) << totaldistance << endl;
 
     return 0;
+
+    
 }
